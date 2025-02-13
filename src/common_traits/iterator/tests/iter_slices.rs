728d183a18ab188c1b0adb9f8ff41b8dd5206a1a
orxfun <orx.ugur.arikan@gmail.com>
2025-02-13 22:38:34 +0100

    empty and fused IterSlices is tested

--- src/common_traits/iterator/tests/iter_slices.rs
+++ src/common_traits/iterator/tests/iter_slices.rs
@@ -18,7 +18,7 @@ fn init_vec<G: Growth>(mut vec: SplitVec<usize, G>, n: usize) -> SplitVec<usize,
 #[test_matrix(
     [SplitVec::with_doubling_growth(), SplitVec::with_linear_growth(2), SplitVec::with_recursive_growth()],
     [0, 1, 3, 4, 5, 8, 27, 185, 446],
-    [0..0, 0..1, 4..4, 4..5, 1..4, 2..6, 2..15, 4..11, 4..12, 4..13, 4..59, 7..28, 7..60, 4..28, 4..60, 0..28, 0..60]
+    [0..0, 0..1, 4..4, 4..5, 1..4, 2..6, 2..15, 4..11, 4..12, 4..13, 4..59, 5..9, 5..12, 7..28, 7..60, 4..28, 4..60, 0..28, 0..60]
 )]
 fn slices_iter(vec: SplitVec<usize, impl Growth>, n: usize, range: Range<usize>) {
     let vec = init_vec(vec, n);
@@ -31,4 +31,29 @@ fn slices_iter(vec: SplitVec<usize, impl Growth>, n: usize, range: Range<usize>)
     let values: Vec<_> = slices.flat_map(|x| x.iter()).copied().collect();
 
     assert_eq!(values, expected);
+}
+
+#[test]
+fn slices_iter_empty() {
+    let mut iter = IterSlices::<u32>::default();
+    assert_eq!(iter.next(), None);
+    assert_eq!(iter.next(), None);
+    assert_eq!(iter.next(), None);
+}
+
+#[test]
+fn slices_iter_fused() {
+    let vec = init_vec(SplitVec::new(), 100);
+    let mut slices = IterSlices::new(&vec, 3..87);
+    let mut expected = 3;
+
+    while let Some(slice) = slices.next() {
+        for x in slice {
+            assert_eq!(*x, expected);
+            expected += 1;
+        }
+    }
+
+    assert!(slices.next().is_none());
+    assert!(slices.next().is_none());
 }
