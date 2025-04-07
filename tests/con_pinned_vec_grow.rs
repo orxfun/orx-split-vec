2ee2d390831fc01e2a2671c2a85cd68d15d0acad
orxfun <orx.ugur.arikan@gmail.com>
2025-04-07 18:17:39 +0200

    con_pin_vec_grow_filled is fixed for linear growth

--- tests/con_pinned_vec_grow.rs
+++ tests/con_pinned_vec_grow.rs
@@ -1,5 +1,3 @@
-use std::arch::x86_64::_CMP_TRUE_UQ;
-
 use orx_split_vec::*;
 
 #[test]
@@ -51,22 +49,28 @@ fn con_pin_vec_grow() {
 }
 
 #[test]
-fn con_pin_vec_grow_filled_xyz() {
+fn con_pin_vec_grow_filled() {
     #[cfg(not(miri))]
     const TARGET_LEN: usize = 35478;
 
     #[cfg(miri)]
     const TARGET_LEN: usize = 37;
 
-    // const TARGET_LEN: usize = 51111;
-
     fn test<G: GrowthWithConstantTimeAccess>(mut vec: SplitVec<String, G>) {
+        vec.reserve_maximum_concurrent_capacity(TARGET_LEN);
+
         assert_eq!(vec.fragments().len(), 1);
 
         let growth = vec.growth().clone();
 
         let mut num_fragments = 1;
         let initial_capacity = growth.fragment_capacity_of(0);
+
+        assert!(
+            initial_capacity < TARGET_LEN,
+            "otherwise, growth test is meaningless"
+        );
+
         let mut capacity = initial_capacity;
 
         // # SAFETY: the vector must have no gaps within its capacity before grow_to_and_fill_with call.
@@ -127,83 +131,12 @@ fn con_pin_vec_grow_filled_xyz() {
         }
     }
 
-    test(SplitVec::with_doubling_growth_and_max_concurrent_capacity());
-    // test(SplitVec::with_linear_growth_and_fragments_capacity(10, 32));
-}
+    // test(SplitVec::with_doubling_growth_and_max_concurrent_capacity());
 
-#[test]
-fn abc() {
-    const LEN: usize = 5;
-
-    let mut vec = SplitVec::with_doubling_growth_and_max_concurrent_capacity();
-
-    let growth = vec.growth().clone();
-
-    let mut num_fragments = 1;
-    let mut capacity = growth.fragment_capacity_of(0);
-    for _ in 0..capacity {
-        vec.push("y".to_string());
-    }
-
-    let con_pinned_vec = vec.into_concurrent();
-
-    let new_fragment_capacity = growth.fragment_capacity_of(num_fragments);
-    let expected_new_capacity = capacity + new_fragment_capacity;
-
-    dbg!(capacity, new_fragment_capacity, expected_new_capacity);
-
-    let new_capacity = con_pinned_vec
-        .grow_to_and_fill_with(capacity + 1, || "x".to_string())
-        .unwrap();
-    assert_eq!(new_capacity, expected_new_capacity);
-    assert_eq!(con_pinned_vec.capacity(), expected_new_capacity);
-
-    for i in capacity..new_capacity {
-        let x = unsafe { con_pinned_vec.get(i) };
-        assert_eq!(x, Some(&"x".to_string()));
-    }
-
-    num_fragments += 1;
-    capacity = new_capacity;
-
-    // let ptr = unsafe { con_pinned_vec.get_ptr_mut(i) };
-    // unsafe { ptr.write(i.to_string()) };
-
-    // for i in 0..LEN {
-    //     if i == capacity {
-    //         let new_fragment_capacity = growth.fragment_capacity_of(num_fragments);
-    //         let expected_new_capacity = capacity + new_fragment_capacity;
-
-    //         let new_capacity = con_pinned_vec
-    //             .grow_to_and_fill_with(capacity + 1, || "x".to_string())
-    //             .unwrap();
-    //         assert_eq!(new_capacity, expected_new_capacity);
-    //         assert_eq!(con_pinned_vec.capacity(), expected_new_capacity);
-
-    //         for i in capacity..new_capacity {
-    //             let x = unsafe { con_pinned_vec.get(i) };
-    //             assert_eq!(x, Some(&"x".to_string()));
-    //         }
-
-    //         num_fragments += 1;
-    //         capacity = new_capacity;
-    //     }
-
-    //     // let ptr = unsafe { con_pinned_vec.get_ptr_mut(i) };
-    //     // unsafe { ptr.write(i.to_string()) };
-    // }
-
-    // for i in 0..LEN {
-    //     let x = unsafe { con_pinned_vec.get(i) };
-    //     assert_eq!(x, Some(&i.to_string()));
-    // }
-
-    let vec = unsafe { con_pinned_vec.into_inner(new_capacity) };
-
-    assert_eq!(vec.len(), new_capacity);
-    for i in 0..vec.len() {
-        // assert_eq!(&vec[i], &i.to_string());
-    }
+    #[cfg(not(miri))]
+    test(SplitVec::with_linear_growth_and_fragments_capacity(10, 32));
+    #[cfg(miri)]
+    test(SplitVec::with_linear_growth_and_fragments_capacity(4, 32));
 }
 
 #[test]
