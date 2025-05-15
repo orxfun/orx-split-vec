90f078d981cf9bbf4ede1a86a204f35c3ea7161c
orxfun <orx.ugur.arikan@gmail.com>
2025-05-15 10:31:39 +0200

    impl JaggedIndexer for Doubling

--- src/concurrent_iter/growths_as_jagged_indexer.rs
+++ src/concurrent_iter/growths_as_jagged_indexer.rs
@@ -1,16 +1,22 @@
-use crate::{Doubling, Growth};
+use crate::{Doubling, GrowthWithConstantTimeAccess};
 use orx_concurrent_iter::implementations::jagged::{JaggedIndex, JaggedIndexer, RawVec};
 
 impl JaggedIndexer for Doubling {
-    fn jagged_index<T>(&self, arrays: &[RawVec<T>], flat_index: usize) -> Option<JaggedIndex> {
-        todo!()
+    fn jagged_index<T>(
+        &self,
+        total_len: usize,
+        _: &[RawVec<T>],
+        flat_index: usize,
+    ) -> Option<JaggedIndex> {
+        (flat_index <= total_len).then(|| {
+            let (f, i) = self.get_fragment_and_inner_indices_unchecked(flat_index);
+            JaggedIndex::new(f, i)
+        })
     }
 
-    unsafe fn jagged_index_unchecked<T>(
-        &self,
-        arrays: &[RawVec<T>],
-        flat_index: usize,
-    ) -> JaggedIndex {
-        todo!()
+    #[inline(always)]
+    unsafe fn jagged_index_unchecked<T>(&self, _: &[RawVec<T>], flat_index: usize) -> JaggedIndex {
+        let (f, i) = self.get_fragment_and_inner_indices_unchecked(flat_index);
+        JaggedIndex::new(f, i)
     }
 }
