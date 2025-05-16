8afa09b4aff4cfd84dd8332d08df703be381f966
orxfun <orx.ugur.arikan@gmail.com>
2025-05-16 10:31:08 +0200

    IntoConcurrentIter for SplitVec<T, G>

---
+++ src/concurrent_iter/con_iter_owned.rs
@@ -0,0 +1,26 @@
+use crate::{Growth, SplitVec};
+use alloc::vec::Vec;
+use orx_concurrent_iter::{
+    IntoConcurrentIter,
+    implementations::jagged_arrays::{ConIterJaggedOwned, JaggedIndexer, RawJagged, RawVec},
+};
+
+impl<T, G> IntoConcurrentIter for SplitVec<T, G>
+where
+    G: Growth + JaggedIndexer,
+    T: Send + Sync,
+{
+    type Item = T;
+
+    type IntoIter = ConIterJaggedOwned<T, G>;
+
+    fn into_con_iter(self) -> Self::IntoIter {
+        let arrays = self
+            .fragments
+            .into_iter()
+            .map(|f| RawVec::from(Vec::from(f)))
+            .collect();
+        let jagged = RawJagged::new(arrays, self.growth, Some(self.len));
+        jagged.into_con_iter()
+    }
+}
--- src/concurrent_iter/mod.rs
+++ src/concurrent_iter/mod.rs
@@ -1,4 +1,4 @@
-// mod con_iter_owned;
+mod con_iter_owned;
 mod con_iter_ref;
 mod growths_as_jagged_indexer;
 
