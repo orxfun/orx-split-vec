897ef02187a323e4bf6596a479dcd466186ac70e
orxfun <orx.ugur.arikan@gmail.com>
2025-09-27 20:58:59 +0200

    ConcurrentSplitVecIntoIter is defined

--- src/concurrent_pinned_vec/into_iter.rs
+++ src/concurrent_pinned_vec/into_iter.rs
@@ -1,6 +1,16 @@
+use crate::{
+    GrowthWithConstantTimeAccess,
+    concurrent_pinned_vec::into_iter_ptr_slices::IntoIterPtrOfConSlices,
+};
 use alloc::vec::Vec;
 use core::cell::UnsafeCell;
 
-pub struct ConcurrentSplitVecIntoIter<T> {
-    data: Vec<UnsafeCell<*mut T>>,
+pub struct ConcurrentSplitVecIntoIter<T, G>
+where
+    G: GrowthWithConstantTimeAccess,
+{
+    slices: IntoIterPtrOfConSlices<T, G>,
+    len_of_remaining_slices: usize,
+    current_ptr: *const T,
+    current_last: *const T,
 }
