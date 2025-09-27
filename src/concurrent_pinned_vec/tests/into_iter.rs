fcd2c02ea95c11db2e898396fbdff9e52dcf75b6
orxfun <orx.ugur.arikan@gmail.com>
2025-09-27 21:58:14 +0200

    into iter tests made generic over growth

--- src/concurrent_pinned_vec/tests/into_iter.rs
+++ src/concurrent_pinned_vec/tests/into_iter.rs
@@ -1,11 +1,29 @@
-use crate::{Doubling, SplitVec, concurrent_pinned_vec::into_iter::ConcurrentSplitVecIntoIter};
+use crate::{
+    Doubling, GrowthWithConstantTimeAccess, Linear, SplitVec,
+    concurrent_pinned_vec::into_iter::ConcurrentSplitVecIntoIter,
+};
 use orx_pinned_vec::{IntoConcurrentPinnedVec, PinnedVec};
-use std::string::ToString;
+use std::string::{String, ToString};
+use test_case::test_matrix;
 
-#[test]
-fn into_iter_non_taken_doubling() {
+fn vec_doubling() -> SplitVec<String, Doubling> {
+    (0..20).map(|x| x.to_string()).collect()
+}
+
+fn vec_linear() -> SplitVec<String, Linear> {
+    let mut vec = SplitVec::with_linear_growth(2);
+    vec.extend((0..20).map(|x| x.to_string()));
+    vec
+}
+
+#[test_matrix([vec_doubling, vec_linear])]
+fn into_iter_non_taken<G, F>(vec: F)
+where
+    G: GrowthWithConstantTimeAccess,
+    F: Fn() -> SplitVec<String, G>,
+{
     let iter = || {
-        let vec: SplitVec<_, Doubling> = (0..20).map(|x| x.to_string()).collect();
+        let vec = vec();
         let range = 0..vec.len();
         let convec = vec.into_concurrent();
         let (growth, data, capacity) = convec.destruct();
