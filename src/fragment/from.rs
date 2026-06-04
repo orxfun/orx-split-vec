b8758297a89cf0edea6a755c9fafaec285b35c75
orxfun <orx.ugur.arikan@gmail.com>
2026-06-04 10:49:31 +0200

    remove dangerous from vec to fragment implementation

--- src/fragment/from.rs
+++ src/fragment/from.rs
@@ -1,11 +1,6 @@
 use crate::Fragment;
 use alloc::vec::Vec;
 
-impl<T> From<Vec<T>> for Fragment<T> {
-    fn from(value: Vec<T>) -> Self {
-        Self { data: value }
-    }
-}
 impl<T> From<Fragment<T>> for Vec<T> {
     fn from(value: Fragment<T>) -> Self {
         value.data
