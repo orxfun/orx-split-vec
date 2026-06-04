b16647c401366105cd512ac0ae0b9d1c0d4f41cf
orxfun <orx.ugur.arikan@gmail.com>
2026-06-04 15:21:40 +0200

    remove from vec into fragment

--- src/fragment/from.rs
+++
@@ -1,8 +0,0 @@
-use crate::Fragment;
-use alloc::vec::Vec;
-
-// impl<T> From<Fragment<T>> for Vec<T> {
-//     fn from(value: Fragment<T>) -> Self {
-//         value.data
-//     }
-// }
--- src/fragment/mod.rs
+++ src/fragment/mod.rs
@@ -2,6 +2,5 @@ mod clone;
 mod debug;
 mod eq;
 pub(crate) mod fragment_struct;
-mod from;
 pub(crate) mod into_fragments;
 pub(crate) mod transformations;
